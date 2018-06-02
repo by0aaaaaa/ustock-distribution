c9f34f8dc48871231bae7638a20c5d9a1343f5e9
benyasin <ben.yasin@gmail.com>
2018-06-02 13:24:08 +0800

    ustock all test pass

--- test/Ustock.test.js
+++ test/Ustock.test.js
@@ -1,60 +1,35 @@
+import EVMRevert from 'openzeppelin-solidity/test/helpers/EVMRevert';
+
+const BigNumber = web3.BigNumber;
+
 const Ustock = artifacts.require('./Ustock.sol');
 const Factory = artifacts.require('./Factory.sol');
 
-// various test utility functions
-const transaction = (address, wei) => ({
-    from: address,
-    value: wei
-});
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
 
 const ethBalance = (address) => web3.eth.getBalance(address).toNumber();
 const toWei = (number) => number * Math.pow(10, 18);
-const fail = (msg) => (error) => assert(false, error ? `${msg}, but got error: ${error.message}` : msg);
-const assertExpectedError = async (promise) => {
-    try {
-        await promise;
-        fail('expected to fail')();
-    } catch (error) {
-        const invalidOpcode = error.message.search('revert') >= -1 || error.message.search('invalid opcode') > -1;
-        console.log(invalidOpcode)
-        assert(invalidOpcode, `Expected throw, but got: ${error.message}`);
-    }
-}
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 const getYmd = (timestamp) => {
     var date = new Date(timestamp * 1000);
     return date.toLocaleString();
 }
 
-const timeController = (() => {
-
-    const addSeconds = (seconds) => new Promise((resolve, reject) =>
-        web3.currentProvider.sendAsync({
-            jsonrpc: "2.0",
-            method: "evm_increaseTime",
-            params: [seconds],
-            id: new Date().getTime()
-        }, (error, result) => error ? reject(error) : resolve(result.result)));
-
-    const addDays = (days) => addSeconds(days * 24 * 60 * 60);
-
-    const currentTimestamp = () => web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-
-    return {
-        addSeconds,
-        addDays,
-        currentTimestamp
-    };
-})();
-
 contract('Ustock', (accounts) => {
 
     const owner = accounts[0];
-    const privateRaiseWallet = accounts[1];
+    const fundsWallet = accounts[1];
+
+    const privateRaiserWallet = accounts[2];
+    const foundationWallet = accounts[3];
 
     let TOTAL_SUPPLY = 1000000000;
 
-    const createToken = () => Ustock.new(owner);
+    const createToken = () => Ustock.new(fundsWallet);
 
     // 发行总量1000000000
     it('should have initial supply of 1000000000 totally', async () => {
@@ -68,185 +43,142 @@ contract('Ustock', (accounts) => {
     // 创始机构持有50%
     it('should have supply of 500000000 units assigned to funds Wallet', async () => {
         const ustock = await createToken();
-        const fundsWalletBalance = await ustock.balanceOf(owner);
+        const ownerWalletBalance = await ustock.balanceOf(owner);
+        const expectedOwnerWalletBalanceTotal = 0;
+
+        const fundsWalletBalance = await ustock.balanceOf(fundsWallet);
         const expectedFundsWalletBalanceTotal = TOTAL_SUPPLY * 0.5;
 
-        //let owner2 = await ustock.owner();
-        //console.log(owner2)
+        //console.log(ownerWalletBalance.toNumber(), fundsWalletBalance.toNumber())
 
+        assert.equal(ownerWalletBalance.toNumber(), expectedOwnerWalletBalanceTotal, 'owner Wallet balance mismatch');
         assert.equal(fundsWalletBalance.toNumber(), expectedFundsWalletBalanceTotal, 'funds Wallet balance mismatch');
     });
 
-    // 关闭前可以转账
-    /*it('should have supply of 5000000000 units assigned to funds Wallet', async () => {
-        const ustock = await createToken();
-        const fundsWalletBalance = await ustock.balanceOf(owner);
-        const expectedFundsWalletBalanceTotal = TOTAL_SUPPLY * 0.5;
-
-        //let owner2 = await ustock.owner();
-        //console.log(owner2)
-
-        assert.equal(fundsWalletBalance.toNumber(), expectedFundsWalletBalanceTotal, 'funds Wallet balance mismatch');
-    });*/
-
-
-
-    /**
-    // REQ002: The ICO is going to last 4 weeks,
-    // trying to raise a minimum of 1,000 ETH and maximum of 20,000 ETH;
-    // if the goal is not met the ICO continues until the payments reach the min cap
-    it('should open at startTimestamp', async () => {
-        const startTimestamp = timeController.currentTimestamp() + 3600;
-        console.log(getYmd(startTimestamp))
-
-        //将开盘时间设为一个小时后开启
-        const ustock = await Ustock.new(fundsWallet, startTimestamp, minCap, maxCap);
-
-        // should be closed
-        await assertExpectedError(ustock.sendTransaction(transaction(buyerOneWallet, oneEth)));
-
-        //时钟拨至一个小时后
-        await timeController.addSeconds(3600);
-
-        // should be open
-        ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
-    });
-
-    it('should last 4 weeks if the goal is reached and allow token transfers afterwards', async () => {
+    // 应该有一个owner
+    it('should have an owner', async () => {
         const ustock = await createToken();
 
-        await ustock.sendTransaction(transaction(buyerOneWallet, minCap));
-
-        const totalRaised = await ustock.totalRaised();
-        console.log(totalRaised.toNumber())
-
-        await timeController.addDays(4 * 7);
-
-        // should be closed
-        await assertExpectedError(ustock.sendTransaction(transaction(buyerTwoWallet, oneEth)));
-
-        const totalRaised2 = await ustock.totalRaised();
-        console.log(totalRaised2.toNumber())
-
-        assert.equal(totalRaised2.toNumber(), minCap, 'Total raised amount mismatch')
-
-        const balance1 = ethBalance(buyerOneWallet);
-        const balance2 = ethBalance(buyerTwoWallet);
-        console.log(balance1, balance2)
-
-        // REQ004 should allow token transfer
-        await ustock.transfer(buyerTwoWallet, 100000000000, {from: buyerOneWallet});
-
-        const balance3 = ethBalance(buyerOneWallet);
-        const balance4 = ethBalance(buyerTwoWallet);
-        console.log(balance3, balance4)
+        let owner = await ustock.owner();
+        //console.log(owner)
+        owner.should.not.eq(ZERO_ADDRESS);
     });
 
-    it('should last more then 4 weeks if the goal is not reached and allow token transfers after closing', async () => {
+    // 可以转移owner
+    it('changes owner after transfer', async function () {
+        const ustock = await createToken();
+        let other = fundsWallet;
+        await ustock.transferOwnership(other);
+        let owner = await ustock.owner();
+
+        owner.should.eq(other);
+    });
+
+    // 应该阻止非owner来进行所有权变更
+    it('should prevent non-owners from transfering', async function () {
         const ustock = await createToken();
 
-        await ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
-        await timeController.addDays(4 * 7 + 1);
-        const totalRaised = await ustock.totalRaised();
-        console.log(totalRaised.toNumber())
+        const other = fundsWallet;
+        const owner = await ustock.owner.call();
 
-        // should still be open
-        await ustock.sendTransaction(transaction(buyerTwoWallet, minCap - oneEth));
-        const totalRaised2 = await ustock.totalRaised();
-        console.log(totalRaised2.toNumber())
+        owner.should.not.eq(other);
 
-        // should be closed
-        await assertExpectedError(ustock.sendTransaction(transaction(buyerThreeWallet, oneEth)));
-        const totalRaised3 = await ustock.totalRaised();
-        console.log(totalRaised3.toNumber())
-
-        assert.equal(totalRaised3.toNumber(), minCap, 'Total raised amount mismatch');
-
-        // REQ004 should allow token transfer
-        await ustock.transfer(buyerTwoWallet, 1, {from: buyerOneWallet});
+        await ustock.transferOwnership(other, {from: other}).should.be.rejectedWith(EVMRevert);
     });
 
-    it('should close after the max cap is reached and allow token transfers afterwards', async () => {
-        const ustock = await createToken();
-        const totalRaised = await ustock.totalRaised();
-        console.log(totalRaised.toNumber())
-
-        await ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
-        const totalRaised2 = await ustock.totalRaised();
-        console.log(totalRaised2.toNumber())
-
-        // should allow going over max cap
-        await ustock.sendTransaction(transaction(buyerTwoWallet, maxCap));
-        const totalRaised3 = await ustock.totalRaised();
-        console.log(totalRaised3.toNumber())
-
-        // should close after reaching max cap
-        await assertExpectedError(ustock.sendTransaction(transaction(buyerThreeWallet, oneEth)));
-
-        const totalRaised4 = await ustock.totalRaised();
-        console.log(totalRaised4.toNumber())
-
-        assert.equal(totalRaised4.toNumber(), maxCap + oneEth, 'Total raised amount mismatch');
-    });
-
-    // REQ003: Raised ether is going to be transferred to a specific wallet after each payment,
-    it('should transfer raised funds to fundsWallet after each payment', async () => {
-        const initialFundsWalletBalance = ethBalance(fundsWallet);
-        const expectedBalanceGrowth = (wei) => initialFundsWalletBalance + wei;
-
+    // 不能将owner权置空
+    it('should guard ownership against stuck state', async function () {
         const ustock = await createToken();
 
-        await ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
-
-        console.log(ustock.address)
-        assert.equal(ethBalance(ustock.address), 0, 'Contract balance mismatch');
-        assert.equal(ethBalance(fundsWallet), expectedBalanceGrowth(oneEth), 'Funds wallet balance mismatch');
-
-        await ustock.sendTransaction(transaction(buyerTwoWallet, oneEth));
-
-        assert.equal(ethBalance(ustock.address), 0, 'Contract balance mismatch');
-        assert.equal(ethBalance(fundsWallet), expectedBalanceGrowth(oneEth * 2), 'Funds wallet balance mismatch');
+        let originalOwner = await ustock.owner();
+        await ustock.transferOwnership(null, {from: originalOwner}).should.be.rejectedWith(EVMRevert);
     });
 
-    // REQ004: Tokens are going to be available for transfers only after the ICO ends,
-    it('should not allow token transfers before ICO', async () => {
-        const startTimestamp = timeController.currentTimestamp() + 3600;
-        const ustock = await Ustock.new(fundsWallet, startTimestamp, minCap, maxCap);
-
-        // should not allow token transfer before ICO
-        await assertExpectedError(ustock.transfer(buyerTwoWallet, 1, {from: buyerOneWallet}));
-    });
-
-    it('should not allow token transfers during ICO', async () => {
+    // 只有owner可以手动关闭合约
+    it('can close the contract manual only by the owner', async function () {
         const ustock = await createToken();
 
-        await ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
-
-        // should not allow token transfer during ICO
-        await assertExpectedError(ustock.transfer(buyerTwoWallet, 1, {from: buyerOneWallet}));
+        await ustock.close({from: owner}).should.be.fulfilled;
+        //const closed = await ustock.closed();
+        //console.log(closed)
+        await ustock.close({from: fundsWallet}).should.be.rejectedWith(EVMRevert);
     });
 
-    // REQ005: The tokens are going to be sold at a flat rate of 1 ETH : 50 ESP,
-    // with added +50% bonus during the first week.
-    it('should be sold with +50% bonus during the first week', async () => {
+    // 合约关闭前可以进行转账主动转账
+    it('can transfer when contract open', async () => {
         const ustock = await createToken();
 
-        await ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
+        await ustock.transfer(privateRaiserWallet, 1000, {from: fundsWallet}).should.be.fulfilled;
 
-        const buyerOneBalance = await ustock.balanceOf(buyerOneWallet);
-        assert.equal(buyerOneBalance.toNumber(), 50 * oneEth * 1.5, 'Buyer one token balance mismatch');
+        const privateRaiserBalance = await ustock.balanceOf(privateRaiserWallet);
+        assert.equal(privateRaiserBalance.toNumber(), 1000, 'privateRaiser Wallet balance mismatch');
     });
 
-    it('should be sold with no bonus after the first week', async () => {
+    // 合约关闭前可以进行转账授权被动转账
+    it('can approve and transferFrom when contract open', async () => {
         const ustock = await createToken();
 
-        await timeController.addDays(8);//7 will fail
-        await ustock.sendTransaction(transaction(buyerOneWallet, oneEth));
+        await ustock.approve(privateRaiserWallet, 1500, {from: fundsWallet});
+        await ustock.transferFrom(fundsWallet, privateRaiserWallet, 1000, {from: privateRaiserWallet}).should.be.fulfilled;
 
-        const buyerOneBalance = await ustock.balanceOf(buyerOneWallet);
+        const privateRaiserBalance = await ustock.balanceOf(privateRaiserWallet);
+        assert.equal(privateRaiserBalance.toNumber(), 1000, 'privateRaiser Wallet balance mismatch');
+    });
 
-        assert.equal(buyerOneBalance.toNumber(), 50 * oneEth, 'Buyer one token balance mismatch');
-    });*/
+    // 合约关闭后不能进行主动转账
+    it('can not transfer when contract closed', async () => {
+        const ustock = await createToken();
+
+        await ustock.close({from: owner});
+        await ustock.transfer(privateRaiserWallet, 1000, {from: fundsWallet}).should.be.rejectedWith(EVMRevert);
+
+        const privateRaiserBalance = await ustock.balanceOf(privateRaiserWallet);
+        assert.equal(privateRaiserBalance.toNumber(), 0, 'privateRaiser Wallet balance mismatch');
+    });
+
+    // 合约关闭后不能进行被动授权转账
+    it('can not approve and transferFrom when contract closed', async () => {
+        const ustock = await createToken();
+        await ustock.close({from: owner});
+
+        await ustock.approve(privateRaiserWallet, 1500, {from: fundsWallet});
+        await ustock.transferFrom(fundsWallet, privateRaiserWallet, 1000, {from: privateRaiserWallet}).should.be.rejectedWith(EVMRevert);
+
+        const privateRaiserBalance = await ustock.balanceOf(privateRaiserWallet);
+        assert.equal(privateRaiserBalance.toNumber(), 0, 'privateRaiser Wallet balance mismatch');
+    });
+
+    // 合约关闭前可以注册
+    it('can register when contract open', async () => {
+        const ustock = await createToken();
+
+        let key1 = 'UTR859gxfnXyUriMgUeThh1fWv3oqcpLFyHa3TfFYC4PK2HqhToVM'
+        let key2 = 'UTR555gxfnXyUriMgUeThh1fWv3oqcpLFyHa3TfFYC4PK2HqhT123'
+
+        await ustock.register(key1, {from: privateRaiserWallet}).should.be.fulfilled;
+        await ustock.register(key2, {from: foundationWallet}).should.be.fulfilled;
+
+        let expectedKey1 = await ustock.keys(privateRaiserWallet)
+        let expectedKey2 = await ustock.keys(foundationWallet)
+        //console.log(expectedKey1, expectedKey2)
+
+        assert.equal(expectedKey1, key1, 'key1 register mismatch');
+        assert.equal(expectedKey2, key2, 'key2 register mismatch');
+    });
+
+    // 合约关闭后不可以继续注册
+    it('can not register when contract closed', async () => {
+        const ustock = await createToken();
+        await ustock.close({from: owner});
+
+        let key1 = 'UTR859gxfnXyUriMgUeThh1fWv3oqcpLFyHa3TfFYC4PK2HqhToVM'
+        await ustock.register(key1, {from: privateRaiserWallet}).should.be.rejectedWith(EVMRevert);
+
+        let expectedKey1 = await ustock.keys(privateRaiserWallet)
+        //console.log(expectedKey1)
+
+        assert.equal(expectedKey1, '', 'key1 register mismatch');
+    });
 
 });
 
